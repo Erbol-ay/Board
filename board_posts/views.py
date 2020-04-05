543462ee6101931fb7b404bad04c25ea949974d2
Erbol-ay <derbolb@gmail.com>
2020-04-05 12:34:42 +0600

    create index view for load indexpage with CBV

--- board_posts/views.py
+++ board_posts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+class IndexPageView(ListView):
+    model = Post
+    template_name = 'index.html'
